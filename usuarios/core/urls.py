5f8fe814d47fed6600ce234f56f4f5e61a23f92f
Juan Andres Roballo <juanroballo85@gmail.com>
2025-06-06 17:02:24 -0300

    usuarios test 2

--- usuarios/core/urls.py
+++ usuarios/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import registrar_usuario, index
+from .views import registrar_usuario, index, obtener_usuario
+
 urlpatterns = [
+    path('', index),
     path('registro/', registrar_usuario),
-    path('', index)
+    path('usuario/<cedula>/', obtener_usuario),
 ]
--- usuarios/core/views.py
+++ usuarios/core/views.py
@@ -3,16 +3,42 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Usuario
 import json
+
+def index(request):
+    return render(request, "index.html")
+
 @csrf_exempt
 def registrar_usuario(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        if Usuario.objects.filter(cedula=data['cedula']).exists():
-            return JsonResponse({'error': 'Cédula ya registrada'}, status=400)
-        if Usuario.objects.filter(email=data['email']).exists():
-            return JsonResponse({'error': 'Email ya registrado'}, status=400)
-        usuario = Usuario.objects.create(**data)
-        return JsonResponse({'status': 'Registrado', 'usuario': usuario.nombre})
+        try:
+            data = json.loads(request.body)
+            cedula = data['cedula']
+            nombre = data['nombre']
+            email = data['email']
+
+            if Usuario.objects.filter(cedula=cedula).exists():
+                return JsonResponse({'error': 'Cédula ya registrada'}, status=400)
+            if Usuario.objects.filter(email=email).exists():
+                return JsonResponse({'error': 'Email ya registrado'}, status=400)
+
+            Usuario.objects.create(cedula=cedula, nombre=nombre, email=email)
+            return JsonResponse({'status': 'Usuario registrado correctamente'})
+
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+
     return JsonResponse({'error': 'Método no permitido'}, status=405)
-def index(request):
-    return render(request, "index.html")
+
+def obtener_usuario(request, cedula):
+    if request.method == 'GET':
+        try:
+            usuario = Usuario.objects.get(cedula=cedula)
+            data = {
+                'cedula': usuario.cedula,
+                'nombre': usuario.nombre,
+                'email': usuario.email
+            }
+            return JsonResponse({'usuario': data})
+        except Usuario.DoesNotExist:
+            return JsonResponse({'error': 'Usuario no encontrado'}, status=404)
+    return JsonResponse({'error': 'Método no permitido'}, status=405)
